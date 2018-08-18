69ef5e34b607b6f5c1b7a32166fdf3af4d697755
Pan Cheng <pchengeducation@hotmail.com>
2018-08-18 02:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function getRepositories(){
   const username = document.getElementById("username").value;
-  
+  const req = name XMLHttpRequest()
+  req.addEventListener("load", displayRepositories);
+  req.open("GET", "https://api.github.com/user/" + username + "/repos")
+  req.send()
 }
+
