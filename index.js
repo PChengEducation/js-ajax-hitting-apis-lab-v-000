6176362f3fb4ae10964aaa5d7148c12ac6aa3045
Pan Cheng <pchengeducation@hotmail.com>
2018-08-18 02:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ function getRepositories(){
   req.send()
 }
 
+function displayRepositories(){
+  const repos = JSON.parse(this.responseText)
+  const repoList = "<ul>" + repos.map(r => {
+    const dataUsername = 'data-username="' + r.owner.login + '"'
+    const dataRepoName = 'data-repository="' + r.name + '"'
+    return(`
+      <li>
+      <h2>${r.name}</h2>
+      <a hreft="${r.html.url}">${r.html_url}</a><br>
+      <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">
+      Get Commits</a><br>
+      <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">
+      Get Branches</a><br></li>
+      </li>`)
+  }).join ('') + "</ul>";
+  document.getElementById("repositories").innerHTML = repoList
+}
